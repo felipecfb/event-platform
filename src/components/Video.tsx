bb608c2da9e3465c0eaa78f5bef97b89bb3c5c2e
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-06-29 15:00:09 -0300

    feat: criando renderização dinâmica do conteúdo via paramétro da url (slug)

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -3,14 +3,59 @@ import { DiscordLogo, Lightning } from "phosphor-react";
 import { ComplementaryCard } from "./ComplementaryCard";
 
 import "@vime/core/themes/default.css";
+import { gql, useQuery } from "@apollo/client";
 
-export function Video() {
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonBySlug($slug: String) {
+    lesson(where: { slug: $slug }) {
+      title
+      videoId
+      description
+      teacher {
+        avatarURL
+        bio
+        name
+      }
+    }
+  }
+`;
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string;
+    videoId: string;
+    description: string;
+    teacher: {
+      avatarURL: string;
+      bio: string;
+      name: string;
+    };
+  };
+}
+
+interface VideoProps {
+  lessonSlug: string;
+}
+
+export function Video(props: VideoProps) {
+  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    variables: { slug: props.lessonSlug },
+  });
+
+  if (!data) {
+    return (
+      <div className="flex-1">
+        <p>Carregando...</p>
+      </div>
+    )
+  }
+
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video bg-red-500">
           <Player>
-            <Youtube videoId="Exg8YBHB02s" />
+            <Youtube videoId={data.lesson.videoId} />
             <DefaultUi />
           </Player>
         </div>
@@ -20,29 +65,25 @@ export function Video() {
         <div className="flex items-start gap-16">
           <div className="flex-1">
             <h1 className="text-2xl font-bold">
-              Aula 01 - Abertura do Ignite Lab
+              {data.lesson.title}
             </h1>
             <p className="mt-4 text-gray-200 leading-relaxed">
-              Nessa aula vamos dar início ao projeto criando a estrutura base da
-              aplicação utilizando ReactJS, Vite e TailwindCSS. Vamos também
-              realizar o setup do nosso projeto no GraphCMS criando as entidades
-              da aplicação e integrando a API GraphQL gerada pela plataforma no
-              nosso front-end utilizando Apollo Client.
+              {data.lesson.description}
             </p>
 
             <div className="flex items-center gap-4 mt-6">
               <img
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src="https://github.com/felipecfb.png"
-                alt=""
+                src={data.lesson.teacher.avatarURL}
+                alt={data.lesson.teacher.name}
               />
 
               <div className="leading-relaxed">
                 <strong className="font-bold text-2xl block">
-                  Felipe Bastos
+                  {data.lesson.teacher.name}
                 </strong>
                 <span className="text-gray-200 text-sm block">
-                  Web Developer Jr.
+                  {data.lesson.teacher.bio}
                 </span>
               </div>
             </div>
